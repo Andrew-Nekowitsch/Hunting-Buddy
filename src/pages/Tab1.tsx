249b731f2f9bbe9ba1a3fd34f7e2f6760890c8fb
James Hilton <MinnesotaJames@users.noreply.github.com>
2019-10-15 00:42:09 -0500

    Dev Changes

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -2,20 +2,18 @@ import {
   IonCard,
   IonCardContent,
   IonCardHeader,
-  IonCardSubtitle,
   IonCardTitle,
   IonContent,
   IonHeader,
-  IonIcon,
-  IonItem,
-  IonLabel,
-  IonList,
-  IonListHeader,
   IonPage,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonIcon
 } from '@ionic/react';
-import { book, build, colorFill, grid } from 'ionicons/icons';
+import { rainy, cloudy, cloudyNight } from 'ionicons/icons';
 import React from 'react';
 import './Tab1.css';
 
@@ -24,45 +22,56 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab One</IonTitle>
+          <IonTitle>Weather</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonCard className="welcome-card">
-          <img src="/assets/shapes.svg" alt="" />
           <IonCardHeader>
-            <IonCardSubtitle>Get Started</IonCardSubtitle>
-            <IonCardTitle>Welcome to Ionic</IonCardTitle>
+            <IonCardTitle>Hourly Tempurature</IonCardTitle>
           </IonCardHeader>
           <IonCardContent>
-            <p>
-              Now that your app has been created, you'll want to start building out features and
-              components. Check out some of the resources below for next steps.
-            </p>
+            <IonGrid>
+              <IonRow class="temps">
+                <IonCol>
+                  <IonIcon icon={rainy} />
+                  <div>
+                    <b>5pm</b><br></br>
+                    56&deg;F/13&deg;C
+                  </div>
+                </IonCol>
+                <IonCol>
+                  <IonIcon icon={rainy} />
+                  <div>
+                    <b>6pm</b><br></br>
+                    52&deg;F/11&deg;C
+                  </div>
+                </IonCol>
+                <IonCol>
+                  <IonIcon icon={cloudy} />
+                  <div>
+                    <b>7pm</b><br></br>
+                    45&deg;F/7&deg;C
+                  </div>
+                </IonCol>
+                <IonCol>
+                  <IonIcon icon={cloudyNight} />
+                  <div>
+                    <b>8pm</b><br></br>
+                    42&deg;F/5&deg;C
+                  </div>
+                </IonCol>
+                <IonCol>
+                  <IonIcon icon={cloudyNight} />
+                  <div>
+                    <b>9pm</b><br></br>
+                    39&deg;F/4&deg;C
+                  </div>
+                </IonCol>
+              </IonRow>
+            </IonGrid>
           </IonCardContent>
         </IonCard>
-
-        <IonList lines="none">
-          <IonListHeader>
-            <IonLabel>Resources</IonLabel>
-          </IonListHeader>
-          <IonItem href="https://ionicframework.com/docs/" target="_blank">
-            <IonIcon slot="start" color="medium" icon={book} />
-            <IonLabel>Ionic Documentation</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/building/scaffolding" target="_blank">
-            <IonIcon slot="start" color="medium" icon={build} />
-            <IonLabel>Scaffold Out Your App</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/layout/structure" target="_blank">
-            <IonIcon slot="start" color="medium" icon={grid} />
-            <IonLabel>Change Your App Layout</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/theming/basics" target="_blank">
-            <IonIcon slot="start" color="medium" icon={colorFill} />
-            <IonLabel>Theme Your App</IonLabel>
-          </IonItem>
-        </IonList>
       </IonContent>
     </IonPage>
   );
