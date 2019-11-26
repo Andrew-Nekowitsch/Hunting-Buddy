c803ce6d5460cc85ea873d7dbc3fe534e9754090
Gavin <34220589+gdutcher100@users.noreply.github.com>
2019-11-25 22:11:35 -0600

    Adds track my location button

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -3,51 +3,67 @@ import {
 	IonToolbar,
 	IonPage,
 	IonTitle,
-	withIonLifeCycle,
-	IonContent
+	IonContent,
+	IonButton
 } from "@ionic/react";
 import React from "react";
 import { loadModules } from "esri-loader";
 import "./Tab2.css";
 
-class Map extends React.Component {
-	mapLoaded: boolean = false;
-	ionViewWillEnter() {
-		if (!this.mapLoaded) {
-			this.mapLoaded = true;
-			loadModules(["esri/views/MapView", "esri/WebMap"])
-				.then(([MapView, ArcGisMap]) => {
-					var webmap = new ArcGisMap({
-						basemap: "topo-vector"
-					});
-					// eslint-disable-next-line
-					var view = new MapView({
-						map: webmap,
-						container: "viewDiv",
-						center: [-91, 46],
-						zoom: 7
-					});
-				})
-				.catch(err => {
-					// handle any errors
-					console.error(err);
-				});
-		}
-	}
-	render() {
-		return (
-			<IonPage>
-				<IonHeader>
-					<IonToolbar>
-						<IonTitle>Topography</IonTitle>
-					</IonToolbar>
-				</IonHeader>
-				<IonContent>
-					<div className="topo-map" id="viewDiv"></div>
-				</IonContent>
-			</IonPage>
-		);
-	}
-}
+loadModules(["esri/views/MapView", "esri/WebMap", "esri/widgets/Track", "esri/widgets/Locate", "esri/Graphic"])
+	.then(([MapView, ArcGisMap, Track, Graphic]) => {
+		var webmap = new ArcGisMap({
+			basemap: "topo-vector"
+		});
+		// eslint-disable-next-line
+		var view = new MapView({
+			map: webmap,
+			container: "viewDiv",
+			center: [-91, 46],
+			zoom: 7,
+			ui: {
+				components: ["defaultUI"]
+			}
+		});
 
-export default withIonLifeCycle(Map);
+		var track = new Track({
+			view: view,
+			container: "trackButton",
+			graphic: new Graphic({
+			  symbol: {
+				type: "simple-marker",
+				size: "12px",
+				color: "green",
+				outline: {
+				  color: "#efefef",
+				  width: "1.5px"
+				}
+			  }
+			}),
+			useHeadingEnabled: false  // Don't change orientation of the map
+		  });
+
+		view.ui.add(track, "top-left");
+	})
+	.catch(err => {
+		// handle any errors
+		console.error(err);
+	});
+
+const Tab2: React.FC = () => {
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle>Topography</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent>
+				<IonButton id="trackButton" shape="round"></IonButton>
+				<div className="topo-map" id="viewDiv"></div>
+			</IonContent>
+		</IonPage>
+	);
+};
+
+export default Tab2;
