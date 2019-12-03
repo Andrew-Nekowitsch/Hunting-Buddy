01fa3f62a0b50e6e3be7b427be05ac0e84a6aeff
Andrew Nekowitsch <anekowitsch@gmail.com>
2019-12-03 09:56:16 -0600

    Map Auto update again

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -3,67 +3,77 @@ import {
 	IonToolbar,
 	IonPage,
 	IonTitle,
-	IonContent,
-	IonButton
+	IonButton,
+	withIonLifeCycle,
+	IonContent
 } from "@ionic/react";
 import React from "react";
 import { loadModules } from "esri-loader";
 import "./Tab2.css";
 
+class Map extends React.Component {
+	mapLoaded: boolean = false;
+	ionViewWillEnter() {
+		if (!this.mapLoaded) {
+			this.mapLoaded = true;
+			
 loadModules(["esri/views/MapView", "esri/WebMap", "esri/widgets/Track", "esri/widgets/Locate", "esri/Graphic"])
-	.then(([MapView, ArcGisMap, Track, Graphic]) => {
-		var webmap = new ArcGisMap({
-			basemap: "topo-vector"
-		});
-		// eslint-disable-next-line
-		var view = new MapView({
-			map: webmap,
-			container: "viewDiv",
-			center: [-91, 46],
-			zoom: 7,
-			ui: {
-				components: ["defaultUI"]
-			}
-		});
-
-		var track = new Track({
-			view: view,
-			container: "trackButton",
-			graphic: new Graphic({
-			  symbol: {
-				type: "simple-marker",
-				size: "12px",
-				color: "green",
-				outline: {
-				  color: "#efefef",
-				  width: "1.5px"
-				}
-			  }
-			}),
-			useHeadingEnabled: false  // Don't change orientation of the map
-		  });
-
-		view.ui.add(track, "top-left");
-	})
-	.catch(err => {
-		// handle any errors
-		console.error(err);
+.then(([MapView, ArcGisMap, Track, Graphic]) => {
+	var webmap = new ArcGisMap({
+		basemap: "topo-vector"
+	});
+	// eslint-disable-next-line
+	var view = new MapView({
+		map: webmap,
+		container: "viewDiv",
+		center: [-91, 46],
+		zoom: 7,
+		ui: {
+			components: ["defaultUI"]
+		}
 	});
 
-const Tab2: React.FC = () => {
-	return (
-		<IonPage>
-			<IonHeader>
-				<IonToolbar>
-					<IonTitle>Topography</IonTitle>
-				</IonToolbar>
-			</IonHeader>
-			<IonContent>
-				<IonButton id="trackButton" shape="round"></IonButton>
-				<div className="topo-map" id="viewDiv"></div>
-			</IonContent>
-		</IonPage>
-	);
-};
+	var track = new Track({
+		view: view,
+		container: "trackButton",
+		graphic: new Graphic({
+		  symbol: {
+			type: "simple-marker",
+			size: "12px",
+			color: "green",
+			outline: {
+			  color: "#efefef",
+			  width: "1.5px"
+			}
+		  }
+		}),
+		useHeadingEnabled: false  // Don't change orientation of the map
+	  });
 
-export default Tab2;
+	view.ui.add(track, "top-left");
+})
+.catch(err => {
+	// handle any errors
+	console.error(err);
+});
+
+		}
+	}
+	render() {
+		return (
+			<IonPage>
+				<IonHeader>
+					<IonToolbar>
+						<IonTitle>Topography</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+				<IonContent>
+					<IonButton id="trackButton" shape="round"></IonButton>
+					<div className="topo-map" id="viewDiv"></div>
+				</IonContent>
+			</IonPage>
+		);
+	}
+}
+
+export default withIonLifeCycle(Map);
